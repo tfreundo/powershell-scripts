2278cdfbd84a9a4c6c04d4f10e2587fa3194e193
Tobias Freundorfer <tobiasfreundorfer@gmail.com>
2019-01-27 10:33:21 +0100

    Added script for fixing OneDrive issue with not connecting to windows

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # powershell-scripts
 Useful powershell scripts for windows automation and bug fixes.
+
+If you have never executed PowerShell scripts on your computer, you first have to allow it using the command (execute this from PowerShell as Administrator!)
+```
+Set-ExecutionPolicy Unrestricted
+```
---
+++ onedrive_connect_to_windows_fix.ps1
@@ -0,0 +1,8 @@
+﻿<#
+    This code fixes OneDrive if it cannot connect to Windows.
+    Execute this script (as administrator) and afterwards restart your computer, the error should be gone.
+
+    See the full support page here: https://support.office.com/de-de/article/fehlermeldung-onedrive-kann-keine-verbindung-zu-windows-herstellen-beim-zugriff-auf-dateien-in-onedrive-7c5b675f-555e-4aa1-b4d8-554698c2c2b9
+
+#>
+Set-Service -Name cldflt -StartupType Automatic
