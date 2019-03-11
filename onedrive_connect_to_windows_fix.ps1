156a7e727656b38c31c30cb079a685f42b3996db
Tobias Freundorfer <tobiasfreundorfer@gmail.com>
2019-03-11 17:35:43 +0100

    Added runas admin

--- onedrive_connect_to_windows_fix.ps1
+++ onedrive_connect_to_windows_fix.ps1
@@ -5,4 +5,11 @@
     See the full support page here: https://support.office.com/de-de/article/fehlermeldung-onedrive-kann-keine-verbindung-zu-windows-herstellen-beim-zugriff-auf-dateien-in-onedrive-7c5b675f-555e-4aa1-b4d8-554698c2c2b9
 
 #>
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+
+{   
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+Break
+}
 Set-Service -Name cldflt -StartupType Automatic
